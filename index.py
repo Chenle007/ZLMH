52bc4be3618dba31e514af09f2cd40f7f2202de0
Chenle007 <2283415024@qq.com>
2020-12-16 17:09:49 +0800

    111

--- index.py
+++ index.py
@@ -1,22 +1,22 @@
-# b=()  #元组类型
-# print(type(b))
-# c=[]  #列表类型
-# print(type(c))
-# d={}  #字典类型
-# print(type(d))
-#
-# scroe = 60
-# if scroe <= 60:
-#     print("成绩不理想哦！")
-#     pass #空语句
-#
-# scroe = int(input("请输入成绩："))
-# if scroe >=90:
-#     print("你的成绩为“优”")
-# elif scroe >=60:
-#     print("你的成绩及格了")
-# else:
-#     print("你没有及格哦")
+b=()  #元组类型
+print(type(b))
+c=[]  #列表类型
+print(type(c))
+d={}  #字典类型
+print(type(d))
+
+scroe = 60
+if scroe <= 60:
+    print("成绩不理想哦！")
+    pass #空语句
+
+scroe = int(input("请输入成绩："))
+if scroe >=90:
+    print("你的成绩为“优”")
+elif scroe >=60:
+    print("你的成绩及格了")
+else:
+    print("你没有及格哦")
 
 import random #导入产生随机数模块
 user = int(input("请出拳 0：石头 1：剪刀 2：布"))
